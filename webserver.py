04398e401de3d33c64e80cd28f2874776669b112
Matteo Lecuit <matteo@lecuit.fr>
2021-03-26 09:01:15 +0100

    update temperature route

--- webserver.py
+++ webserver.py
@@ -1,32 +1,37 @@
-from flask import Flask, render_template, request
-import serial, json
+from flask import Flask, render_template, request, jsonify
+import serial
+import json
 
 app = Flask(__name__)
 # initialize Serial
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 ser.flush()
 
+
 @app.route('/temperature', methods=['GET'])
 def temperature():
     data = raspberry_data()
-    return render_template('temperature.html', data=data)
+    return jsonify(data)
 
 
-@app.route('/light', methods=['POST'])
+@app.route('/light', methods=['GET', 'POST'])
 def light():
-    data = []
-    return render_template('light.html', data=data)
+    if request.method == 'GET':
+        data = {"light": "0"}
+        return render_template('light.html', data=data)
+    if request.method == 'POST':
+        return True
+
 
 def raspberry_data():
- if ser.in_waiting > 0:
-  jsonString = ser.readline().decode('utf-8').rstrip()
-  try:
-   jsonObject = json.loads(jsonString)
-   return jsonObject["lumen"]
-  except:
-   pass
- return -1
-
+    if ser.in_waiting > 0:
+        jsonString = ser.readline().decode('utf-8').rstrip()
+        try:
+            jsonObject = json.loads(jsonString)
+            return jsonObject["lumen"]
+        except:
+            pass
+    return -1
 
 
 app.run(host='localhost', port=5000)
