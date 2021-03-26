36803a98240888da26c265f69f57123d955acd46
Paul <paulereverend@gmail.com>
2021-03-26 08:48:37 +0100

    add raspberry data

--- webserver.py
+++ webserver.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
+import serial, json
 
 app = Flask(__name__)
 
 
 @app.route('/temperature', methods=['GET'])
 def temperature():
-    data = []
+    data = raspberry_data()
     return render_template('temperature.html', data=data)
 
 
@@ -13,6 +14,17 @@ def temperature():
 def light():
     data = []
     return render_template('light.html', data=data)
+
+def raspberry_data():
+ if ser.in_waiting > 0:
+  jsonString = ser.readline().decode('utf-8').rstrip()
+  try:
+   jsonObject = json.loads(jsonString)
+   return jsonObject["lumen"]
+  except:
+   pass
+ return -1
+
 
 
 app.run(host='localhost', port=5000)
